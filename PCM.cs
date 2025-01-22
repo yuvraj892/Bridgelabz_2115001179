77ea25f795ff19f911664b146bcb334084591218
yuvraj892 <yuvrajsrivastava892@gmail.com>
2025-01-22 16:47:52 +0530

    added PCM file

---
+++ PCM.cs
@@ -0,0 +1,10 @@
+using System;
+    class PCM{
+        static void Main(){
+            int physicsMarks = 95;
+            int chemistryMarks = 96;
+            int mathsMarks = 94; 
+            int avgMarks = (physicsMarks + chemistryMarks+ mathsMarks)/3;
+            Console.WriteLine(" Sam's average mark in PCM is " +avgMarks);
+        }
+    }
