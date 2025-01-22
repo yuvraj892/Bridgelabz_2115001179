54e92c8e7bddc1f9a7acd901aa8cd5e1f8c78c6a
yuvraj892 <yuvrajsrivastava892@gmail.com>
2025-01-22 16:49:19 +0530

    added kilometerintomiles file

---
+++ KilometersintoMiles.cs
@@ -0,0 +1,9 @@
+using System;
+    class KilometersIntoMiles{
+        static void Main(){
+            double kilometers =10.8;
+            double milesInKilometer = 1.6;
+            double Calculate = kilometers*milesInKilometer;
+            Console.WriteLine("The distance "+ kilometers + "km in miles is " +Calculate);
+        }
+    } 
