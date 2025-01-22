68feccce5782fb8f78598a21b869bdc681000b66
yuvraj892 <yuvrajsrivastava892@gmail.com>
2025-01-22 16:41:22 +0530

    added age file

---
+++ age.cs
@@ -0,0 +1,14 @@
+using System;
+
+class age {
+	static void Main(){
+		int curYr=2024;
+		int birYr=2000;
+		
+		Console.WriteLine("Harry's age in 2024  is "+Age(curYr,birYr));
+	}	
+  static int Age(int curYr,int birYr){
+	return  curYr-birYr;
+	
+  }
+}
