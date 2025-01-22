a59e6b6c577646a717d8f23fa5a2cdeb5d3ead6b
yuvraj892 <yuvrajsrivastava892@gmail.com>
2025-01-22 16:58:54 +0530

    added height file

---
+++ height.cs
@@ -0,0 +1,11 @@
+using System;
+    class program
+	{
+        static void Main(){
+            Console.Write("Enter your height in cm: ");
+            double height= Convert.ToInt32(Console.ReadLine());
+            double heightInInches= height/2.54;
+			double heigtInFoots= heightInInches/12;
+			Console.WriteLine("Your Height in cm is "+ height+" while in feet is "+heigtInFoots+" and inches is " + heightInInches);
+        }
+    }
